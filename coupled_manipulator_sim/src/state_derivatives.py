c4268b7dd518ce1eaedb5a5639441cbe2ee009b2
MaEl1405 <mahdielmi20001411@gmail.com>
2025-11-01 19:07:16 +0330

    feat: Implement 8x8 matrix for coupled inverse dynamics

--- coupled_manipulator_sim/src/state_derivatives.py
+++ coupled_manipulator_sim/src/state_derivatives.py
@@ -40,16 +40,36 @@ def state_derivatives(state, robots, controllers, phase_offsets, dt, t_offset):
     M_master, CG_master, _ = master_arm.forward_dynamics(q_master, q_dot_master, tau=np.zeros(2))
     J_dot_master = master_arm.get_jacob_dot(q_master[0], q_master[1], q_dot_master)
     
-    # Desired accelerations 
-    q_ddot_master_desired = np.linalg.solve(J_master, desired_acc - J_dot_master @ q_dot_master)
-    constraint_term = J_master @ q_ddot_master_desired + J_dot_master @ q_dot_master - J_dot_slave @ q_dot_slave
-    q_ddot_slave_required = np.linalg.solve(J_slave, constraint_term)
+    # Desired accelerations
+    zero_matrix_2x2 = np.zeros([2,2])
+    identity_matrix_2x2 = np.eye(2)
+
+    # Solve Control Equations
+    coef_matrix = np.block([
+        [J_master, zero_matrix_2x2, zero_matrix_2x2, zero_matrix_2x2],
+        [zero_matrix_2x2, J_slave, zero_matrix_2x2, zero_matrix_2x2],
+        [zero_matrix_2x2, M_slave, -J_slave.T, zero_matrix_2x2],
+        [M_master, zero_matrix_2x2, J_master.T, -identity_matrix_2x2]
+    ])
+
+    b_matrix = np.block([
+        [desired_acc.reshape(2,1) - (J_dot_master@q_dot_master).reshape(2,1)],
+        [desired_acc.reshape(2,1) - J_dot_slave@q_dot_slave.reshape(2,1)],
+        [-CG_slave.reshape(2,1) ],
+        [-CG_master.reshape(2,1) ]
+    ])
+
+    control_solution = np.linalg.solve(coef_matrix, b_matrix)
+
+    # Desired joint accelerations
+    q_ddot_master_desired = control_solution[0:2]
+    q_ddot_slave_required = control_solution[2:4]
 
     # Desired master torque (compensate dynamics + constraint force)
-    lambda_force_required = np.linalg.solve(J_slave.T, M_slave @ q_ddot_slave_required + CG_slave)
-    tau_joints_master = (M_master @ q_ddot_master_desired) + CG_master + (J_master.T @ lambda_force_required)
-    tau_joints_slave = np.zeros(2)
-
+    lambda_force_required = control_solution[4:6]
+    tau_joints_master = control_solution[6:8]
+    tau_joints_slave = np.zeros([2,1])
+    
     ## Calculate constraints Error 
     P_master = np.array(master_arm.forward_kinematics(q_master[0], q_master[1])) # master end-effector pos
     P_slave  = np.array(slave_arm.forward_kinematics(q_slave[0], q_slave[1]))    # Slave end-effector pos
@@ -59,27 +79,26 @@ def state_derivatives(state, robots, controllers, phase_offsets, dt, t_offset):
     e_P = P_master - P_slave  # end effector position error
     e_V = V_master - V_slave  # End effector Velocity error
 
-    ## Define K and W matrix (K@X = W) Plant Modeling
+    ## Solve Plantt Equations
     zeros_2x2 = np.zeros((2,2))
     K_row1 = np.hstack([M_master, zeros_2x2, J_master.T])
     K_row2 = np.hstack([zeros_2x2, M_slave, -J_slave.T])
     K_row3 = np.hstack([J_master, -J_slave, zeros_2x2])
     K = np.vstack([K_row1, K_row2, K_row3])
 
-
-    W_row1 = tau_joints_master - CG_master
-    W_row2 = tau_joints_slave -CG_slave
+    # Ordinate variables
+    W_row1 = tau_joints_master - CG_master.reshape(2,1)
+    W_row2 = tau_joints_slave -CG_slave.reshape(2,1)
     W_row3 = J_dot_slave@q_dot_slave - J_dot_master@q_dot_master - (alpha_stabilize * e_P) -(beta_stabilize * e_V)
-
-    W = np.vstack([W_row1.reshape(2,1), W_row2.reshape(2,1), W_row3.reshape(2,1)])
+    W = np.vstack([W_row1, W_row2, W_row3.reshape(2,1)])
 
     # Solve for accelerations and forces
-    X = np.linalg.solve(K, W)  # X = [qddot_mater qddot_slave lambda]
+    plant_solution = np.linalg.solve(K, W)  # X = [qddot_mater qddot_slave lambda]
 
     ## Solve Equations
-    qddot_master = X[0:2]
-    qddot_slave  = X[2:4]
-    lambda_force = X[4:6]
+    qddot_master = plant_solution[0:2]
+    qddot_slave  = plant_solution[2:4]
+    lambda_force = plant_solution[4:6]
 
     # Concatenate all flat arrays
     state_derivative = np.concatenate([
